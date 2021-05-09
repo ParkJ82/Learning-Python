632e06ff2909bfd91fdb2f29288c28a3e0bae906
박정우 (Park) <79061791+ParkJ82@users.noreply.github.com>
2021-05-09 19:50:26 -0400

    Update Number 1.py

--- Problem Set 1/Number 1.py
+++ Problem Set 1/Number 1.py
@@ -1,3 +1,9 @@
+"""You have graduated from MIT and now have a great job! You move to the San Francisco Bay Area and
+decide that you want to start saving to buy a house.  As housing prices are very high in the Bay Area,
+you realize you are going to have to save for several years before you can afford to make the down
+payment on a house. In Part A, we are going to determine how long it will take you to save enough
+money to make the down payment given the following assumptions"""
+
 total_cost = float(input('Cost of dream home: '))
 portion_saved = float(input('Portion of salary to be saved: '))
 annual_salary = float(input('Starting annual salary: '))
